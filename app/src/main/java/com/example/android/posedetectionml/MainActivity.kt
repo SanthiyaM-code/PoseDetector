bf51464119c0df4b4af59d3e127f05f1c1e5bd78
Vibin <vibin.cr22@gmail.com>
2021-05-09 13:43:59 +0530

    Posedetection object returns non empty list

--- app/src/main/java/com/example/android/posedetectionml/MainActivity.kt
+++ app/src/main/java/com/example/android/posedetectionml/MainActivity.kt
@@ -12,7 +12,12 @@ import android.util.Log
 import android.widget.Toast
 import androidx.core.content.FileProvider
 import com.example.android.posedetectionml.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.Task
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.pose.Pose
 import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.PoseLandmark
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions
 import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
 import java.io.File
 import java.io.IOException
@@ -28,14 +33,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val options = PoseDetectorOptions.Builder()
-            .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
-            .build()
-        val poseDetector = PoseDetection.getClient(options)
-
+//        val options = PoseDetectorOptions.Builder()
+//            .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
+//            .build()
         binding.takepic.setOnClickListener {
             dispatchTakePictureIntent()
         }
+
 
     }
 
@@ -88,8 +92,63 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             binding.imageview.setImageURI(photoUri)
-        }
 
+            val options = AccuratePoseDetectorOptions.Builder()
+                .setDetectorMode(AccuratePoseDetectorOptions.SINGLE_IMAGE_MODE)
+                .build()
+
+            val poseDetector = PoseDetection.getClient(options)
+
+            val image = InputImage.fromFilePath(applicationContext, photoUri)
+            val taskpose = poseDetector.process(image).addOnSuccessListener {
+                Toast.makeText(this, "Task Successful", Toast.LENGTH_SHORT).show()
+                val pose = it
+                val allPoseLandmarks = pose.allPoseLandmarks
+                Log.d("POSEMAN",allPoseLandmarks.toString())
+
+                val leftShoulder = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)
+                val rightShoulder = pose.getPoseLandmark(PoseLandmark.RIGHT_SHOULDER)
+                val leftElbow = pose.getPoseLandmark(PoseLandmark.LEFT_ELBOW)
+                val rightElbow = pose.getPoseLandmark(PoseLandmark.RIGHT_ELBOW)
+                val leftWrist = pose.getPoseLandmark(PoseLandmark.LEFT_WRIST)
+                val rightWrist = pose.getPoseLandmark(PoseLandmark.RIGHT_WRIST)
+                val leftHip = pose.getPoseLandmark(PoseLandmark.LEFT_HIP)
+                val rightHip = pose.getPoseLandmark(PoseLandmark.RIGHT_HIP)
+                val leftKnee = pose.getPoseLandmark(PoseLandmark.LEFT_KNEE)
+                val rightKnee = pose.getPoseLandmark(PoseLandmark.RIGHT_KNEE)
+                val leftAnkle = pose.getPoseLandmark(PoseLandmark.LEFT_ANKLE)
+                val rightAnkle = pose.getPoseLandmark(PoseLandmark.RIGHT_ANKLE)
+                val leftPinky = pose.getPoseLandmark(PoseLandmark.LEFT_PINKY)
+                val rightPinky = pose.getPoseLandmark(PoseLandmark.RIGHT_PINKY)
+                val leftIndex = pose.getPoseLandmark(PoseLandmark.LEFT_INDEX)
+                val rightIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_INDEX)
+                val leftThumb = pose.getPoseLandmark(PoseLandmark.LEFT_THUMB)
+                val rightThumb = pose.getPoseLandmark(PoseLandmark.RIGHT_THUMB)
+                val leftHeel = pose.getPoseLandmark(PoseLandmark.LEFT_HEEL)
+                val rightHeel = pose.getPoseLandmark(PoseLandmark.RIGHT_HEEL)
+                val leftFootIndex = pose.getPoseLandmark(PoseLandmark.LEFT_FOOT_INDEX)
+                val rightFootIndex = pose.getPoseLandmark(PoseLandmark.RIGHT_FOOT_INDEX)
+                val nose = pose.getPoseLandmark(PoseLandmark.NOSE)
+                val leftEyeInner = pose.getPoseLandmark(PoseLandmark.LEFT_EYE_INNER)
+                val leftEye = pose.getPoseLandmark(PoseLandmark.LEFT_EYE)
+                val leftEyeOuter = pose.getPoseLandmark(PoseLandmark.LEFT_EYE_OUTER)
+                val rightEyeInner = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE_INNER)
+                val rightEye = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE)
+                val rightEyeOuter = pose.getPoseLandmark(PoseLandmark.RIGHT_EYE_OUTER)
+                val leftEar = pose.getPoseLandmark(PoseLandmark.LEFT_EAR)
+                val rightEar = pose.getPoseLandmark(PoseLandmark.RIGHT_EAR)
+                val leftMouth = pose.getPoseLandmark(PoseLandmark.LEFT_MOUTH)
+                val rightMouth = pose.getPoseLandmark(PoseLandmark.RIGHT_MOUTH)
+
+
+            }.addOnFailureListener {
+                Toast.makeText(this, "Task Failure", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+
+        }
     }
 
 
